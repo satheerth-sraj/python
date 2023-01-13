43d89be310ea65701bbf4c2c63cf16a724065b6a
satheerth-sraj <122590884+satheerth-sraj@users.noreply.github.com>
2023-01-13 12:08:53 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,13 @@
+a=int(input("enter the first number"))
+b=int(input("enter the second number"))
+op=(input("enter the opretor")
+if(op=="+"):
+    print(a+b)
+elif(op=="-")
+    print(a-b)
+elif(op=="*")
+    print(a*b)
+elif(op=="/")
+    print(a/b)
+else:
+    print("invalied")
